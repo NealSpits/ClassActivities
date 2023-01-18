1472187b9249546219a6fd8f3bdbccf793223eae
William Pruden <willpruden@gmail.com>
2023-01-17 22:50:30 -0500

    2.2.2

---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a constant variable (const)
+const myvariable = "love";
+// create a variable that can be reassigned (let)
+let age;
+// create three variables and assign them values of different data types (=)
+let myname = "William";
+let age = "3";
+let isnotcool = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof age);
+console.log(typeof myname);
+// change the value referenced by a variable (dynamic typing)
+console.log(isnotcool);
+isnotcool = 18;
+// print the type of the variable you just changed (typeof)
+let myname ="William";
+let language = "Javascript";
+let weeks = 20;
+let masterStatus = false;
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log(
+  "Hello, my name is " +
+  myname + ",
+  and I am learning " +
+  language + ".
+  I have been practicing for only " +
+   weeks + " weeks, so my status as a master is: " +
+   masterStatus + ".");
+
+
