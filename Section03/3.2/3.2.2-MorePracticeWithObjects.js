8182b7784a3e2db7b397153be84667438374deb6
William Pruden <willpruden@gmail.com>
2023-01-31 20:30:27 -0500

    completed section 3 activities

---
+++ Section03/3.2/3.2.2-MorePracticeWithObjects.js
@@ -0,0 +1,46 @@
+/*
+  3.2.2-MorePracticeWithObjects.js
+  ========================================
+*/
+
+// bracket and dot notation
+//-------------------------
+
+
+// what will the following lines print?
+
+/*
+Create an object with at least four properties, each with a different data type
+Name one of the four properties "collection" and set its value to an Array or Object
+*/
+
+
+// access a value in in "collection" property
+
+
+// Nested Arrays and Objects
+//===========================
+
+// below is an array of strings with 5 elements
+
+// below is an object with 2x key-value pairs
+
+// adding a method to the object `newObj`
+
+
+// add a nested object key-value pair to the object `newObj`
+// fill in your favorite movie and color below
+
+
+// add a complex array to the object `newObj`
+
+
+/*
+How many lines will the following for...of statement print?
+What do you expect to see on each line?
+*/
+
+
+// use a template literal to print a sentence about your favorite movie and color
+
+// access the values "b", 4, and 6 from obj.list
